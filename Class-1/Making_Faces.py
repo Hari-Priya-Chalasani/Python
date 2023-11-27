c7bf55c98eb8bc8de45655a204e91688e68068de
Hari Priya Chalasani <priyachalasani1@gmail.com>
2023-11-27 10:27:14 +0000

    Mon, Nov 27, 2023, 3:57 PM +05:30

--- Class-1/Making_Faces.py
+++ Class-1/Making_Faces.py
@@ -1,3 +1,8 @@
-word = input("Enter the text.  ")
-word = word.replace(":)", "🙂").replace(":(", "🙁")
-print(word)
+word = input("Enter the text: ")
+new_word = word.replace(":)", "🙂").replace(":(", "🙁")
+
+# Check if any replacements were made
+if new_word != word:
+    print("Replaced:", new_word)
+else:
+    print("No replacements made. Original text:", word)
