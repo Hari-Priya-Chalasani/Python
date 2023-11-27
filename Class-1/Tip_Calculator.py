e1b30aaba275e8b82ad928c11ca7c7aefcfb7751
Hari Priya Chalasani <priyachalasani1@gmail.com>
2023-11-27 10:57:20 +0000

    Mon, Nov 27, 2023, 4:27 PM +05:30

---
+++ Class-1/Tip_Calculator.py
@@ -0,0 +1,10 @@
+def main():
+    amount = input("How much was the meal? $")
+    percentage = input("What percentage would you like to tip?  ")
+    tip = amount * percentage
+    print(f"Leave:{tip:.2f}")
+def amount():
+    amount = float(amount)
+    return amount
+def percentage():
+    percentage = (percentage/)
